cd3dde8056e5fd95ec03228bb095d3415cf43ad0
andrewlee977 <andrew.lee97@gmail.com>
2021-05-17 17:05:18 -0700

    insert more users

--- twitoff/app.py
+++ twitoff/app.py
@@ -26,7 +26,7 @@ def create_app():
 
     @app.route('/populate')
     def populate():
-        insert_users(["elonmusk", "jackblack"])
+        insert_users(["elonmusk", "jackblack", "jeffbezos", "brianchesky"])
         users = User.query.all()
         return render_template("base.html", title="Home", users=users)
 
